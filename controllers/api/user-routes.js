de8d751628108fd315014569895e82d015e1db95
Jacob Canepa <jacobmcanepa@gmail.com>
2022-10-04 20:59:01 -0700

    set up basic user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,29 @@
+const { User } = require('../../models');
 const router = require('express').Router();
 
+// GET /api/users
 router.get('/', (req, res) => {
-  res.json({ message: 'success' });
+  User.findAll()
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST /api/users
+router.post('/', (req, res) => {
+  /* expects:
+  username: <username>,
+  email: <email>,
+  password: <password>
+  */
+  User.create(req.body)
+    .then(data => res.json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
