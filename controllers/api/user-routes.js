3de81c71eec5a28ad16f45ee83d6e57754425178
Jacob Canepa <jacobmcanepa@gmail.com>
2022-10-06 19:51:52 -0700

    added delete route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -45,4 +45,24 @@ router.post('/', (req, res) => {
     });
 });
 
+// DELETE /api/users/:id
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
