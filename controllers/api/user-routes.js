1139d4ca7d88d0a962af9ce233ab42c85f01139e
Jacob Canepa <jacobmcanepa@gmail.com>
2022-10-06 19:40:26 -0700

    added GET user by id route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -11,13 +11,32 @@ router.get('/', (req, res) => {
     });
 });
 
+// GET /api/users/:id
+router.get('/:id', (req, res) => {
+  User.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 // POST /api/users
 router.post('/', (req, res) => {
-  /* expects:
-  username: <username>,
-  email: <email>,
-  password: <password>
-  */
+  // expects...
+  // username: <username>,
+  // email: <email>,
+  // password: <password>
   User.create(req.body)
     .then(data => res.json(data))
     .catch(err => {
