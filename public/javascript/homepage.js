32ec28bb58fae1f9ce0bf35ff16634969cc392eb
wwartick <willwartick@gmail.com>
2022-10-11 19:26:06 -0700

    added front end html

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -1,3 +1,25 @@
+async function addMovie(event) {
+  event.preventDefault();
+
+  const user_id = document.querySelector('#user_id').innerHTML.trim();
+  const title = document.querySelector('#title').innerHTML.trim();
+
+  const response = await fetch('/api/movies', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      title,
+      user_id
+    })
+  });
+
+  if (response.ok) {
+    console.log('success!');
+  } else {
+    document.location.replace('/login');
+  }
+}
+
 async function homeFormHandler(event) {
   event.preventDefault();
 
@@ -20,7 +42,18 @@ async function homeFormHandler(event) {
 
   const suggestions = document.getElementById('suggestions');
   suggestions.innerHTML = '';
-  suggestions.innerHTML = data.result[0].text;
+
+  const spanEl = document.createElement('span');
+  spanEl.setAttribute('id', 'title');
+  spanEl.classList.add('text-light', 'font-weight-bold', 'pr-3', 'movie-text');
+  spanEl.innerHTML = data.result[0].text;
+  suggestions.appendChild(spanEl);
+  
+  const addButton = document.createElement('button');
+  addButton.classList.add('btn', 'bg-success', 'text-light');
+  addButton.innerHTML = 'Add Movie';
+  suggestions.appendChild(addButton);
+  addButton.addEventListener('click', addMovie);
 };
 
 document.querySelector('#home-form').addEventListener('submit', homeFormHandler);
