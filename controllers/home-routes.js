cc905db1ba48f02be75b0a0858db8944d4a44b28
wwartick <willwartick@gmail.com>
2022-10-13 02:00:29 -0700

    cleanup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,14 +36,20 @@ router.get('/profile', (req,res) => {
    });
   });
 
-
-
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
     return;
   }
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
 });
 
 
